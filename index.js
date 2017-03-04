7942793894a0434615857e70327443d351bbebad
florian leroux <f.j.j.leroux@gmail.com>
2017-03-04 16:14:44 +0100

    add slick carousel and personnalisation

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
-	$('.image').html('<img src=""/>')
-
-
-
-
-
-
-
-
+	$(".single-item").slick({
+		dots: true,
+		arrows: true,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		infinite: true,
+		centerMode: true,
+		variableWidth: true,
+	});
+	$('.container').css({
+		background: 'gray',
+		'padding-top': '15px',
+	});
+	$('img').css('width', '400px');
 });
