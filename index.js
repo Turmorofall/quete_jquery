3d2f89c721e69d2c28e5e8302c13507b82c83fe5
florian leroux <f.j.j.leroux@gmail.com>
2017-03-04 16:48:54 +0100

    add main.css + change pictures

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
 	$(".single-item").slick({
-		dots: true,
-		arrows: true,
-		slidesToShow: 1,
 		autoplay: true,
 		autoplaySpeed: 3000,
+		arrows: true,
+		dots: true,
 		infinite: true,
-		centerMode: true,
-		variableWidth: true,
+		adaptativeHeight: true,
+		fade: true,
 	});
-	$('.container').css({
-		background: 'gray',
-		'padding-top': '15px',
-	});
-	$('img').css('width', '400px');
 });
